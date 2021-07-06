1d65b51daf27bd8c1aaae81dc78607ec50097f3c
Cole Lawrence <cole@colelawrence.com>
2021-07-06 16:58:07 -0400

    Variable length prefix

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
+# HintedID
+
+Adds `hintedid` type to PostgreSQL and the `generate_hinted_id(text)` function.
+
+Hinted IDs are a 20 character ID inspired by Stripe's ID format:
+
+ - variable length text prefix
+ - `_`
+ - 20 character base32 XID using [xid](https://github.com/kazk/xid-rs)
+
+```sql
+SELECT generate_hinted_id('user');
+   generate_hinted_id    
+-------------------------
+ user_c3ibeg2ciaeol4vj3p0g
+(1 row)
+
+SELECT generate_hinted_id('user');
+   generate_hinted_id    
+-------------------------
+ user_c3ibeg2ciaeol4vj3p0g
+(1 row)
+```
+
 ### Contributing
 
 Must have `cargo-pgx` to have `cargo pgx init` run so the build scripts work.
--- src/hinted_id.rs
+++ src/hinted_id.rs
@@ -1,72 +1,59 @@
 use ::xid::{self as xid_};
 use pgx::*;
-use std::convert::TryInto;
+use serde::*;
 use std::ffi::CStr;
 use std::str::FromStr;
 
-#[derive(Copy, Clone, PostgresType)]
-#[pgvarlena_inoutfuncs]
+type InlineShortString = smartstring::SmartString<smartstring::Compact>;
+
+/// standard Rust equality/comparison derives
+#[derive(Eq, PartialEq, Ord, Hash, PartialOrd)]
+/// Pg type
+#[derive(PostgresType, Serialize, Deserialize)]
+/// automatically generate =, <> SQL operator functions
+#[derive(PostgresEq)]
+/// automatically generate <, >, <=, >=, and a "_cmp" SQL functions
+/// When "PostgresEq" is also derived, pgx also creates an "opclass" (and family)
+/// so that the type can be used in indexes `USING btree`
+#[derive(PostgresOrd)]
+/// automatically generate a "_hash" function, and the necessary "opclass" (and family)
+/// so the type can also be used in indexes `USING hash`
+#[derive(PostgresHash)]
+/// Goodies
+#[derive(Clone)]
+#[repr(C)]
+#[inoutfuncs]
 pub struct HintedID {
-    prefix: [u8; 2],
-    xid: xid_::Id,
-}
-
-impl PgVarlenaInOutFuncs for HintedID {
-    fn input(input: &CStr) -> PgVarlena<Self> {
-        let mut result = PgVarlena::<HintedID>::new();
-        let input_str = input.to_str().expect("valid string");
-        // 20 + 2 + 1 (_) = 23
-        assert_eq!(input_str.len(), 23, "hintedid expected to be 23 characters");
-
-        if let [p1, p2, b'_', ..] = input_str.as_bytes() {
-            result.prefix = [*p1, *p2];
-            result.xid =
-                xid_::Id::from_str(&input_str[3..]).expect("hintedid had invalid XID component");
-            result
-        } else {
-            panic!("hintedid expected to have a prefix");
-        }
-    }
-
-    fn output(&self, buffer: &mut StringInfo) {
-        buffer.push_bytes(&self.prefix);
-        buffer.push('_');
-        buffer.push_str(&self.xid.to_string());
-    }
+    // `xid` ordered first so xid is checked first for ordering
+    xid: [u8; 12],
+    prefix: InlineShortString,
 }
 
 #[pg_extern]
 fn generate_hinted_id(prefix: &str) -> HintedID {
-    assert_eq!(
-        prefix.len(),
-        2,
-        "hinted_id needs a prefix of exactly two characters"
-    );
     HintedID {
-        prefix: prefix.as_bytes().try_into().unwrap(),
-        xid: xid_::new(),
+        prefix: prefix.into(),
+        xid: xid_::new().0,
     }
 }
 
-impl<'a> IntoDatum for &'a HintedID {
-    #[inline]
-    fn into_datum(self) -> Option<pg_sys::Datum> {
-        let HintedID { prefix, xid } = self;
-        let collected = prefix
-            .into_iter()
-            .chain(&xid.0)
-            .copied()
-            .collect::<Vec<_>>();
-        let varlena = rust_byte_slice_to_bytea(collected.as_slice());
-        if varlena.is_null() {
-            None
-        } else {
-            Some(varlena.into_pg() as pg_sys::Datum)
+impl InOutFuncs for HintedID {
+    fn input(input: &CStr) -> Self {
+        let input_str = input.to_str().expect("valid string");
+        let (prefix, xid_str) = input_str
+            .split_once('_')
+            .expect("hintedid expected to have an underscore _ between a prefix and an xid");
+        HintedID {
+            prefix: prefix.into(),
+            xid: xid_::Id::from_str(&xid_str)
+                .expect("hintedid had invalid XID component")
+                .0,
         }
     }
 
-    #[inline]
-    fn type_oid() -> u32 {
-        pg_sys::BYTEAOID
+    fn output(&self, buffer: &mut StringInfo) {
+        buffer.push_str(&self.prefix);
+        buffer.push('_');
+        buffer.push_str(&xid_::Id(self.xid).to_string());
     }
 }
